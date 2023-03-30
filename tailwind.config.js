b0c400024fe62811bb41f81cb5a856854dbe3404
kdlibiran <kglibiran@up.edu.ph>
2023-03-30 10:44:04 +0800

    Setup Changes

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ['class'],
+  content: ["./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{html,js}",
+    "./node_modules/tw-elements/dist/js/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("tw-elements/dist/plugin")],
+}
