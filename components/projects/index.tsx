9d41de54b0eb59b1c97ad1ea8972b913b63580ad
kdlibiran <kglibiran@up.edu.ph>
2024-02-25 21:17:21 +0800

    Responsive design for projects

--- components/projects/index.tsx
+++ components/projects/index.tsx
@@ -4,11 +4,11 @@ export default function ProjectsPage() {
   return (
     <div
       id="projects"
-      className="center top-0 flex h-[100vh] flex-1 snap-center overflow-hidden bg-stone-200"
+      className="center top-0 flex flex-1 snap-center overflow-hidden bg-stone-200"
     >
       <div className="mt-32 flex w-[100vw] flex-initial flex-col items-center">
         <h1 className="text-4xl sm:text-6xl">Projects</h1>
-        <div className="flex flex-1 flex-row gap-10">
+        <div className="flex flex-1 flex-col gap-10 p-10 sm:flex-row sm:p-0">
           <Project
             title="FreeWall"
             body="An open source social media apps where there are different walls and you can post anything on any wall anonymously."
