4e717896dac33233bb711894f09f428ecc74c4b6
kdlibiran <kglibiran@up.edu.ph>
2024-03-16 14:14:42 +0800

    Update: Design for the project page

--- components/projects/index.tsx
+++ components/projects/index.tsx
@@ -1,53 +1,42 @@
-import { Project } from "../ui/project";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
+import { HeroParallax } from "../ui/hero-parallax";
 
 export default function ProjectsPage() {
+  const products = [
+    {
+      title: "SariTracker",
+      link: "https://sari-tracker.vercel.app",
+      thumbnail: "/static/images/saritracker.png",
+    },
+    {
+      title: "FreeWall",
+      link: "https://freewall.vercel.app",
+      thumbnail: "/static/images/freewall.png",
+    },
+    {
+      title: "Food Price Predictor",
+      link: "https://github.com/kdlibiran/foodpricepredictor",
+      thumbnail: "/static/images/fpp.png",
+    },
+    {
+      title: "SariTracker",
+      link: "https://sari-tracker.vercel.app",
+      thumbnail: "/static/images/saritracker.png",
+    },
+    {
+      title: "FreeWall",
+      link: "https://freewall.vercel.app",
+      thumbnail: "/static/images/freewall.png",
+    },
+    {
+      title: "Food Price Predictor",
+      link: "https://github.com/kdlibiran/foodpricepredictor",
+      thumbnail: "/static/images/fpp.png",
+    },
+  ];
+
   return (
-    <div
-      id="projects"
-      className="center top-0 flex flex-1 snap-center overflow-hidden bg-stone-200 pt-[72px] sm:h-[100vh]"
-    >
-      <div className="flex w-[100vw] flex-initial flex-col items-center sm:mt-10">
-        <h1 className="text-4xl sm:text-6xl">Projects</h1>
-        <div className="px-30 -mt-10 flex flex-1 flex-col justify-center sm:flex-row sm:gap-x-10 sm:px-0">
-          <Carousel className="w-[90vw] sm:w-auto sm:max-w-screen-sm">
-            <CarouselContent>
-              <CarouselItem>
-                <Project
-                  title="FreeWall"
-                  body="An open source social media apps where there are different walls and you can post anything on any wall anonymously."
-                  image="/static/images/freewall.png"
-                  link="https://freewall.vercel.app/"
-                />
-              </CarouselItem>
-              <CarouselItem>
-                <Project
-                  title="SariTracker"
-                  body="A web app that serves as an inventory management system for store owners. It also has a feature that lets consumers conveniently view the inventory of nearby sari-sari stores"
-                  image="/static/images/saritracker.png"
-                  link="https://sari-tracker.vercel.app"
-                />
-              </CarouselItem>
-              <CarouselItem>
-                <Project
-                  title="Food Price Predictor"
-                  body="A web app that utilizes a regression model which predicts the prices of different kinds of food in different places in the Philippines."
-                  image="/static/images/fpp.png"
-                  link="https://github.com/kdlibiran/foodpricepredictor"
-                />
-              </CarouselItem>
-            </CarouselContent>
-            <CarouselPrevious className="hidden bg-black text-white sm:flex" />
-            <CarouselNext className="hidden bg-black text-white sm:flex" />
-          </Carousel>
-        </div>
-      </div>
+    <div id="projects">
+      <HeroParallax products={products} />
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,9 @@
 import type { Config } from "tailwindcss";
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 const config = {
   darkMode: ["class"],
@@ -74,7 +79,22 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("tailwind-scrollbar-hide")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("tailwind-scrollbar-hide"),
+    addVariablesForColors,
+  ],
 } satisfies Config;
 
 export default config;
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val]),
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
