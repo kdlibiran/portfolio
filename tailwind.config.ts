9045c25a2f61486a9aaccd9fdc1ec7ce9afdbf9e
kdlibiran <kyle.libiran@enstack.com>
2024-08-18 01:01:02 +0800

    Fix: errors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,13 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
-const config: Config = {
+const config = {
   darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
-  ],
+	],
   prefix: "",
   theme: {
     container: {
@@ -74,20 +74,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [
-    require("tailwindcss-animate"),
-    function({ addUtilities }) {
-      addUtilities({
-        '.scrollbar-hide': {
-          'scrollbar-width': 'none', /* Firefox */
-          '-ms-overflow-style': 'none', /* Internet Explorer and Edge */
-        },
-        '.scrollbar-hide::-webkit-scrollbar': {
-          display: 'none', /* Chrome, Safari */
-        },
-      }, ['responsive', 'hover']);
-    },
-  ],
-};
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
 
-export default config;
+export default config
